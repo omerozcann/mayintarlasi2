21552ea634d2570582063df89ca13e28fce7cc6a
Öğr.Gör.Ömer Özcan <69793480+omerozcann@users.noreply.github.com>
2023-12-07 15:46:20 +0300

    Proje dosyası ekle.

---
+++ mayintarlasiGorsel1/mayinFrm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace mayintarlasiGorsel1
+{
+    partial class mayinFrm
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.baslat = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // baslat
+            // 
+            this.baslat.Location = new System.Drawing.Point(155, 21);
+            this.baslat.Name = "baslat";
+            this.baslat.Size = new System.Drawing.Size(75, 23);
+            this.baslat.TabIndex = 0;
+            this.baslat.Text = "Başlat";
+            this.baslat.UseVisualStyleBackColor = true;
+            this.baslat.Click += new System.EventHandler(this.baslat_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(50, 50);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(280, 270);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Oyun Alanı";
+            // 
+            // mayinFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 379);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.baslat);
+            this.Name = "mayinFrm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mayın Tarlası V1.0";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button baslat;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ mayintarlasiGorsel1/mayinFrm.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mayintarlasiGorsel1
+{
+    public partial class mayinFrm : Form
+    {
+        int[,]oyunAlani=new int[10,10];
+        Button[,]butonlar=new Button[10,10];
+        public mayinFrm()
+        {
+            InitializeComponent();
+        }
+
+        public void mayinDose()
+        {
+            Random r=new Random();
+            for (int j = 0; j < 10; j++)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    oyunAlani[i, j] = 0;
+                }
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                oyunAlani[r.Next(10), r.Next(10)] = 1;
+            }
+        }
+        private void baslat_Click(object sender, EventArgs e)
+        {
+            butonlariSil();
+            mayinDose();
+            for (int j = 0; j < 10; j++)
+            {
+                for (int i = 0; i < 10; i++)
+                {                    
+                    Button b = new Button();
+                    b.Parent = groupBox1;// this deyimi mayinFrm formunu gösterir
+                    b.Width = 20; // butonun genişliği
+                    b.Height = 20;//butonun yüksekliği
+                    b.Left =5+20 * i;// butonun yataydaki konumu
+                    b.Top =15+20*j;//butonun dikeydeki konumu
+                    b.Text = "";
+                    b.Name = i+"-"+j;
+                    b.Click += oyunbtn_Click;// üretilen butona tıklanınca merhababtn_click olayını çağır
+                    butonlar[i,j] = b;
+                }
+            }   
+            
+        }
+
+        public void butonlariSil()
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    if (butonlar[i, j] != null)
+                    {
+                        butonlar[i, j].Dispose();
+                    }
+                }
+            }
+        }
+        public void mayinlariGoster()
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    if (oyunAlani[i,j]==1)
+                    {
+                        butonlar[i, j].BackgroundImage = Image.FromFile("mayin.png");
+                        butonlar[i, j].BackgroundImageLayout = ImageLayout.Stretch;
+                    }
+                }
+            }
+        }
+        public int mayinSay(int x,int y)
+        {
+            int mayinSayisi = 0;
+            if (x < 9)
+            {
+                if (oyunAlani[x + 1, y] == 1)
+                {
+                    mayinSayisi++;
+                }
+            }
+            if (x < 9 && y < 9)
+            {
+                if (oyunAlani[x + 1, y + 1] == 1)
+                {
+                    mayinSayisi++;
+                }
+            }
+            if (y < 9)
+            {
+                if (oyunAlani[x, y + 1] == 1)
+                {
+                    mayinSayisi++;
+                }
+            }
+            if (x > 0 && y < 9)
+            {
+                if (oyunAlani[x - 1, y + 1] == 1)
+                {
+                    mayinSayisi++;
+                }
+            }
+            if (x > 0)
+            {
+                if (oyunAlani[x - 1, y] == 1)
+                {
+                    mayinSayisi++;
+                }
+            }
+            if (x > 0 && y > 0)
+            {
+                if (oyunAlani[x - 1, y - 1] == 1)
+                {
+                    mayinSayisi++;
+                }
+            }
+            if (y > 0)
+            {
+                if (oyunAlani[x, y - 1] == 1)
+                {
+                    mayinSayisi++;
+                }
+            }
+            if (x < 9 && y > 0)
+            {
+                if (oyunAlani[x + 1, y - 1] == 1)
+                {
+                    mayinSayisi++;
+                }
+            }
+            return mayinSayisi;
+        }
+        private void oyunbtn_Click(object sender, EventArgs e)
+        {
+            Button b=(Button)sender;
+            String[] d = b.Name.Split('-');
+            int x = int.Parse(d[0]);
+            int y = int.Parse(d[1]);
+            if (oyunAlani[x,y]==1)
+            {
+                mayinlariGoster();
+            }
+            else
+            {
+                b.BackColor= Color.White;
+                if (oyunAlani[x+1,y]!=1&&x<9)
+                {
+                    butonlar[x + 1, y].Text = mayinSay(x + 1, y).ToString();
+                }
+                if (oyunAlani[x + 1, y + 1] != 1 && x < 9 && y < 9)
+                {
+                    butonlar[x + 1, y + 1].Text = mayinSay(x + 1, y + 1).ToString();
+                }
+                if (oyunAlani[x, y + 1] != 1 && y < 9)
+                {
+                    butonlar[x, y + 1].Text = mayinSay(x + 1, y).ToString();
+                }
+                if (oyunAlani[x - 1, y + 1] != 1 && x > 0 && y < 9)
+                {
+                    butonlar[x - 1, y + 1].Text = mayinSay(x + 1, y).ToString();
+                }
+                if (oyunAlani[x - 1, y] != 1 && x > 0)
+                {
+                    butonlar[x - 1, y].Text = mayinSay(x + 1, y).ToString();
+                }
+                if (oyunAlani[x - 1, y - 1] != 1 && x > 0 && y > 0)
+                {
+                    butonlar[x - 1, y - 1].Text = mayinSay(x + 1, y).ToString();
+                }
+                if (oyunAlani[x, y - 1] != 1 && y > 0)
+                {
+                    butonlar[x, y - 1].Text = mayinSay(x + 1, y).ToString();
+                }
+                if (oyunAlani[x + 1, y-1] != 1 && x < 9&&y>0)
+                {
+                    butonlar[x + 1, y - 1].Text = mayinSay(x + 1, y).ToString();
+                }
+            }
+        }
+    }
+}
